67c9ce35f4d8abf0e25ebd2c51d06f74a010621f
unknown <pedrohematos@outlook.com>
2021-02-22 16:18:35 -0300

    initial default theme switched to dark-mode and grammar mistakes corrected

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,11 @@
 /* CssSwitcher js */
-function handleThemeSwitcher(){
-
-  const cssStyle = document.getElementById('style');
-  const themeSwitcher = document.getElementById('themeSwitcher');
+function handleThemeSwitcher() {
+  const cssStyle = document.getElementById("style");
+  const themeSwitcher = document.getElementById("themeSwitcher");
 
   if (themeSwitcher.checked) {
+    cssStyle.href = "assets/css/themes.light.css";
+  } else {
     cssStyle.href = "assets/css/themes.dark.css";
   }
-  else {
-    cssStyle.href = "assets/css/themes.light.css";
-  }
-};
+}
